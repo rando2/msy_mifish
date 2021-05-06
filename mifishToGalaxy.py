cf4e171e36223db47e688adb2ae39460a8165ef8
HM Rando <halie.rando@pennmedicine.upenn.edu>
2021-05-06 13:35:01 -0400

    add read taxonomy to create .tax

--- mifishToGalaxy.py
+++ mifishToGalaxy.py
@@ -1,12 +1,15 @@
 # Before running, must install python packages openpyxl and pandas packages through PyCharm
 import pandas as pd
 import os
+import csv
 
 # SET YOUR VARIABLES HERE
 # Set the name of your mifish output file (this assume it's in the same directory (folder) as this script)
 mifish_file = "mifish.xlsx"
-# Set the name of the associated taxonomy file
+# Set the name of the information about the taxonomy
 taxonomy_file = "fish_taxonomy.xlsx"
+# Set the name of the output (.tax) file for galaxy
+output_file = "mifish.tax"
 
 # THESE ARE FUNCTIONS YOU WON'T NEED TO CHANGE
 def create_taxonomy(mifish_data, taxonomy_file):
@@ -19,26 +22,54 @@ def create_taxonomy(mifish_data, taxonomy_file):
     taxonomy["Kingdom"] = ["Animalia"] * len(taxonomy.index)
     taxonomy["Phylum"] = [""] * len(taxonomy.index) # We may be able to set this to chordata, just depends on what Mifish is looking for
     taxonomy["Class"] = [""] * len(taxonomy.index)
+    taxonomy["Mifish species"] = taxonomy["Species"]
     taxonomy["Genus"] = [x[0] for x in taxonomy["Species"].str.split(" ")]
-    taxonomy["Species"]  = [x[0] for x in taxonomy["Species"].str.split(" ")]
+    taxonomy["Species"] = [x[1] for x in taxonomy["Species"].str.split(" ")]
 
-    taxonomy[["Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"]].to_excel(taxonomy_file)
+    taxonomy[["Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "mifish_species"]].to_excel(taxonomy_file, index=False)
     print("Fish taxonomy is in", taxonomy_file, ". Please fill in taxonomic information")
-    exit(0)
+    exit("Exiting after creating taxonomy file")
 
 # HERE IS THE MAIN SCRIPT WITH STEPS
 # First, read in an Excel file from mifish to Python
 mifish_data = pd.read_excel(mifish_file)
 
-# Check if the taxonomic information is available & filled in. If the file doesn't exist, generate it.
+# Load (or generate) taxonomic info file, depending on whether it exists
 if os.path.exists(taxonomy_file):
+    # Check if the taxonomic information in the file is filled in
     mifish_tax = pd.read_excel(taxonomy_file, na_filter = True)
     if mifish_tax["Phylum"].isna().sum() > 0:
         exit("Error: You still need to fill in the phylogenetic information in " + taxonomy_file)
     else:
-        print(list(mifish_tax["Phylum"]))
+        with open(output_file, "w") as outfile:
+            out_writer = csv.writer(outfile, delimiter="\t")
+            # If the info is filled in, we can build our .tax file
+            for index, row in mifish_data.iterrows():
+                sample_name = row["Sample name"]
+                confidence = row["Confidence"]
+                mifish_species = row["Species"]
+                tax_row = mifish_tax[mifish_tax['Mifish species'] == mifish_species].values[0]
+                kingdom, phylum, pclass, order, family, genus, species, mifish_species = tax_row
+                tax_string = kingdom + "(100);" + phylum + "(100);" + pclass + \
+                             "(100);" + order + "(100);"
+                if confidence == "HIGH":
+                    tax_string += family + "(100);" + genus + "(95);" + \
+                                 species + "(92);"
+                elif confidence == "MODERATE":
+                    tax_string += family + "(97);" + genus + "(87);" + \
+                                 species + "(80);"
+                elif confidence == "LOW":
+                    tax_string += family + "(92);" + genus + "(82);" + \
+                                 species + "(70);"
+                else:
+                    exit("Error! Confidence value not recognized: " + confidence)
+                print(tax_string)
+                # Write the output to a file in .tax format
+                out_writer.writerow([sample_name, tax_string])
+
 else:
+    # If the file doesn't exist, generate it.
     create_taxonomy(mifish_data, taxonomy_file)
 
-# Taxonomy file: This file is formatted to show the sample name and then the taxonomic classification, like this:
+# Taxonomy file: This file is formatted to show the sample name and then the taxonomic classification, like this
 # 794_HC2_R_001.fastq	Bacteria(100);Cyanobacteria_Chloroplast(98);Chloroplast(96);Chloroplast_order_incertae_sedis(96);Chloroplast(96);Bacillariophyta(88);
